8029d3c3fd8c88f4569a87f07fc62ef6bcb0e249
Rakshit Vallabhaneni <rakshit.svce@gmail.com>
2021-06-07 18:29:07 -0400

    Pizza Cart

---
+++ ShopCart/obj/Release/netcoreapp3.1/PubTmp/EFSQLScripts/PizzaCart.Data.PizzaCartContext.sql
@@ -0,0 +1,34 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210607211538_init')
+BEGIN
+    CREATE TABLE [Product] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NULL,
+        [Description] nvarchar(max) NULL,
+        [Type] nvarchar(max) NULL,
+        [Photo] nvarchar(max) NULL,
+        [Price] float NOT NULL,
+        CONSTRAINT [PK_Product] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210607211538_init')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210607211538_init', N'3.1.15');
+END;
+
+GO
+
